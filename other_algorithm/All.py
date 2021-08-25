8726317cc08dfb7e4ef790c0a74935a482eca98b
rexrex9 <547102444@qq.com>
2021-08-25 09:47:04 +0800

    GPU

--- other_algorithm/All.py
+++ other_algorithm/All.py
@@ -1,21 +1,22 @@
 from major import GAFM_user,GAFM_base,GAFM_item
-from other_algorithm import A1_LR,A2_ALS,B1_FM,B2_AFM,C1_FNN,C2_Deep_FM,D2_GAT4Rec
+from other_algorithm import A1_LR, A2_ALS, B1_FM, B2_AFM, C1_FNN, C2_Deep_FM, D2_GAT4Rec
 
 
 #datas = ['mlLatest','ml1m','Bx']
-datas = ['ml1m','ml10m']
-Models = [A1_LR,A2_ALS,B1_FM,B2_AFM,C1_FNN,C2_Deep_FM,D2_GAT4Rec,GAFM_base,GAFM_item,GAFM_user]
+datas = [ 'ml1m', 'ml10m' ]
+Models = [ D2_GAT4Rec, GAFM_base, GAFM_item, GAFM_user ]
+
 #Models = [D1_GCN4Rec,D2_GAT4Rec]
-def doOne(Model):
-    print(Model)
+
+def doOne( Model ):
+    print( Model )
     for d in datas:
-        print(d)
-        Model.train(d,epochs=10,need_eva=False)
+        print( d )
+        Model.train( d, epochs = 10, need_eva = False )
 
-def do():
+def do( ):
     for one in Models:
-        doOne(one)
-
+        doOne( one )
 
 if __name__ == '__main__':
-    do()
+    do( )
